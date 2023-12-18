088397be8af51a8e28f285f635f6f9e2b6f28d5c
PremKumarAddala <127920285+PremKumarAddala@users.noreply.github.com>
2023-12-18 11:30:52 +0530

    task1.js

--- task1.js
+++ task1.js
@@ -1,34 +1,39 @@
-const ak = require('ak');
-const m= require('m');
-const bodyParser=require('body-parser');
-const app = ak();
-const PORT = 3000;
-m.connect('mongodb://localhost:27017/registration', { useNewUrlParser: true, useUnifiedTopology: true });
-const userSchema = new m.Schema({
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName', { useNewUrlParser: true, useUnifiedTopology: true });
+const User = mongoose.model('User', {
   username: String,
   email: String,
   password: String
 });
-const User = m.model('User', userSchema);
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(ak.static('public'));
+app.use(express.static(__dirname));
+app.use(express.json());
+
+// Route to render the registration form
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/registration.html');
 });
-app.post('/register', (req, res) => {
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    });
-    newUser.save((err) => {
-      if (err) {
-        res.send('Error registering user.');
-      } else {
-        res.send('User registered successfully!');
-      }
-    });
-  });
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+
+// Route to handle form submission and store data in MongoDB
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.send('Registration successful!');
+  } catch (err) {
+    res.status(500).send('Error registering user');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
